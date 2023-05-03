3fef2468e8902221b6bf6d56cc5f34c3995d0c03
Maximilian <scientificresearch309@gmail.com>
2023-05-03 23:39:02 +0200

    display the Jobs in PageJob.tsx

--- src/pages/PageJobs.tsx
+++ src/pages/PageJobs.tsx
@@ -1,7 +1,26 @@
+import jobs from '../data/jobs.json';
+
 export const PageJobs = () => {
 	return (
 		<div className="page pageJobs">
-			<p>This is the Jobs page.</p>
+			<div className="jobs">
+				<h2>There are {jobs.length} jobs:</h2>
+				{jobs.map((job) => {
+					return (
+						<div className="job">
+							<div className="title">
+								<a href={job.url} target="_blank">
+									{job.title}
+								</a>
+							</div>
+							<div className="company">{job.company}</div>
+							<div className="description">{job.description}</div>
+							<div className="skills">{job.skillList}</div>
+							<div className="todo">{job.todo}</div>
+						</div>
+					);
+				})}
+			</div>{' '}
 		</div>
 	);
 };
